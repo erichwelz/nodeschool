305910f6acb4f9beeec20b0d4ca025e218c693e9
Erich Welz <erichwelz@gmail.com>
2017-03-17 15:46:26 -0400

    Clean up Nav

--- 05filteredls.js
+++ 05filteredls.js
@@ -1,17 +1,14 @@
 var fs = require('fs')
-    path = require('path')
 
-let filePath = process.argv[2]
-let ext = process.argv[3]
-
+let dir = process.argv[2]
+let ext = '.' + process.argv[3]
 let extensionRegex = new RegExp(`${ext}$`)
 
-console.log(`path: ${filePath}\nextension: ${ext}`)
-
-fs.readdir(filePath, (err, files) => {
+fs.readdir(dir, (err, files) => {
   if (err) {
     throw err;
   }
   let filtered = files.filter((file => extensionRegex.test(file) ))
-  console.log(filtered)
+  let lineSeparated = filtered.toString().replace(/,/g,'\n')
+  console.log(lineSeparated)
 })
