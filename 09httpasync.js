03133a2b47b3f43cf9d0563e9a25a2094da915a5
Erich Welz <erichwelz@gmail.com>
2017-03-21 17:16:20 -0400

    Cleaner sync solution with queue

--- 09httpasync.js
+++ 09httpasync.js
@@ -3,21 +3,19 @@ var bl = require('bl')
 
 let urls = process.argv.slice(2, 5)
 
-let urlsLeft = urls.length
-
 function callUrl (url) {
   http.get(url, (res) => {
     res.pipe(bl(
       (err, data) => {
       data = data.toString()
       console.log(data)
-      urlsLeft --
-      if ( urlsLeft > 0 ) {
-        callUrl(urls[3 - urlsLeft])
+      urls.shift()
+      if ( urls.length > 0 ) {
+        callUrl(urls[0])
       }
       })
     )
   })
 }
 
-callUrl(urls[3 - urlsLeft])
+callUrl(urls[0])
