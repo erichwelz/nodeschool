543b3f4a9e75363960b72dc2f171beaec372b2c5
Erich Welz <erichwelz@gmail.com>
2017-03-23 11:23:42 -0400

    ES6 time server

--- 10timeserver.js
+++ 10timeserver.js
@@ -8,5 +8,8 @@ var server = net.createServer((socket) => {
 server.listen(portNumber)
 
 const time = () => {
-  return "2013-07-06 17:42\n"
+  let date = new Date
+  // not accounting for Nov/Dec.. would probably used strftime library if for Prod use
+  return date.getFullYear() + '-0' + ( date.getMonth() + 1 )+ '-' + date.getDate() + ' ' +
+  date.getHours() + ':' + date.getMinutes() + '\n'
 }
