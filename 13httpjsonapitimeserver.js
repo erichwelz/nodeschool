26259b4fdfad752c51264b422962b73efa58d0b7
Erich Welz <erichwelz@gmail.com>
2017-03-24 10:58:44 -0400

    Solution for 13

--- 13httpjsonapitimeserver.js
+++ 13httpjsonapitimeserver.js
@@ -3,35 +3,35 @@ var url = require('url')
 let portNumber = process.argv[2]
 
 var server = http.createServer((req, res) => {
-    let urlObj = url.parse(req.url, true)
+  let parsedUrl = url.parse(req.url, true)
+  let time = new Date(parsedUrl.query.iso)
 
-    if ( urlObj.pathname == '/api/parsetime' ) {
-      if ( "iso" in urlObj.query ) {
-        var result = formatIsoTime(urlObj.query.iso)
-      }
-    }
+  if ( parsedUrl.pathname == '/api/parsetime' ) {
+    var result = formatIsoTime(time)
+  }
 
-    if ( urlObj.pathname == '/api/unixtime' ) {
-      if ( "iso" in urlObj.query ) {
-        var result = formatUnixTime(urlObj.query.iso)
-      }
-    }
+  if ( parsedUrl.pathname == '/api/unixtime' ) {
+    var result = formatUnixTime(time)
+  }
 
+  if ( result ) {
     res.writeHead(200, { 'Content-Type': 'application/json' })
     res.end(JSON.stringify(result))
+  } else {
+    res.writeHead(404)
+    res.end()
+  }
 })
 server.listen(portNumber)
 
-let formatIsoTime = (timeIn) => {
-  let DateObj = new Date(timeIn)
-  let timeOut = {}
-  timeOut.hour = DateObj.getHours()
-  timeOut.minute = DateObj.getMinutes()
-  timeOut.second = DateObj.getSeconds()
-  return timeOut
+let formatIsoTime = (time) => {
+  return {
+    hour: time.getHours(),
+    minute: time.getMinutes(),
+    second: time.getSeconds()
+  }
 }
 
-let formatUnixTime = (timeIn) => {
-  let DateObj = new Date(timeIn)
-  return { DateOBj.getTime()
+let formatUnixTime = (time) => {
+  return { unixtime: time.getTime() }
 }
